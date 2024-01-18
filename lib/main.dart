503cbbce0b0101b149131dca7a3db6666d3c228e
bhtu <bhtu.work@gmail.com>
2024-01-18 20:54:50 +0700

    move components into shared

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/app_bar.dart';
-import 'package:flutter_application_1/app_drawer.dart';
+import 'package:flutter_application_1/share/components/app_bar.dart';
+import 'package:flutter_application_1/share/components/app_drawer.dart';
 
 void main() {
   runApp(const MyApp());
